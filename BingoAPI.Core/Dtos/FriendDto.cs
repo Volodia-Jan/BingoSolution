8d7760696d7d74b7b4ea01af7b0b181e4a1be9f0
Volodymyr Hula <volodia.gula@ukr.net>
2023-02-19 21:52:21 +0200

    Added friendship DTO

---
+++ BingoAPI.Core/Dtos/FriendDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BingoAPI.Core.Dtos;
+public class FriendDto
+{
+    [Required(ErrorMessage = "{0} can not be empty")]
+    [DisplayName("Friend username")]
+    public string? FriendUserName { get; set; }
+}
