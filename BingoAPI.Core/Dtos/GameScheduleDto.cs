df1175a9b26372b4c6c9d17a4dce4ea5a74bd0d2
Volodymyr Hula <volodia.gula@ukr.net>
2023-02-19 21:52:37 +0200

    Added game schedule DTO

---
+++ BingoAPI.Core/Dtos/GameScheduleDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BingoAPI.Core.Dtos;
+public class GameScheduleDto
+{
+    [Required(ErrorMessage = "{0} can not be empty")]
+    [DisplayName("Game Time")]
+    public string? GameTime { get; set; }
+}
