7f98ae37df11b66f74047eb3401d680c1b05d2ce
Volodymyr Hula <volodia.gula@ukr.net>
2023-02-19 21:48:23 +0200

    Added friendship entity

---
+++ BingoAPI.Core/Entities/Friendship.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BingoAPI.Core.Entities;
+public class Friendship
+{
+    public Guid UserId { get; set; }
+    public Guid FriendId { get; set; }
+    public bool IsAccepted { get; set; }
+
+    [ForeignKey("UserId")]
+    public virtual ApplicationUser? User { get; set; }
+
+    [ForeignKey("FriendId")]
+    public virtual ApplicationUser? Friend { get; set; }
+}
