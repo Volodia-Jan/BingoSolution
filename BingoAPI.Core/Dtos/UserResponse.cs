03d6f8cbb126053f078a94aec395b7d8db78b286
Volodymyr Hula <volodia.gula@ukr.net>
2023-02-19 21:51:59 +0200

    Update application user DTO

--- BingoAPI.Core/Dtos/UserResponse.cs
+++ BingoAPI.Core/Dtos/UserResponse.cs
@@ -5,5 +5,6 @@ public class UserResponse
     public string? FullName { get; set; }
     public string? UserName { get; set; }
     public string? PhoneNumber { get; set; }
+    public bool IsGameInfoPrivate { get; set; }
     public DateTime? GameSchedule { get; set; }
 }
